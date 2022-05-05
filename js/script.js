04b4fa9c58bbcc5dc41427c0d5f35da72f57f400
Gregory N. Mayer <gregory280@hotmail.com>
2022-05-05 19:05:58 -0300

    add webpack and babel

--- js/script.js
+++ js/script.js
@@ -3,31 +3,15 @@ import initAnimationScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordian.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFetchRanking from './modules/fetch-ranking.js';
-
-
 
 initScrollSuave();
 initAnimationScroll();
 initAccordion();
 initTabNav();
 initModal();
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFetchRanking();
-
-
-
-
- 
-
-
-
-
-
-
-

7412eba14148ab3307ba8030f4f9d923431de5cc
Gregory N. Mayer <gregory280@hotmail.com>
2022-05-06 18:07:40 -0300

    refactoring tabnav

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,20 +1,31 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('active');
-    });
-    tabContent[index].classList.add('active');
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'active';
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('active');
-    tabMenu.forEach((item, index) => {
-      item.addEventListener('click', () => {
-        activeTab(index);
-      });
+  // Ativa a tab de acordo com o index da mesma
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
+    });
+    this.tabContent[index].classList.add(this.activeClass);
+  }
+
+  // Método para iniciar
+  init() {
+    // Ativará o tab se o array (conteúdo) existir
+    if (this.tabMenu.length && this.tabContent.length) {
+      this.activeTab(0); // Ativa o primeiro item
+      this.addTabNavEvent();
+    }
+  }
+
+  // Método para adicionar os eventos nas tabs
+  addTabNavEvent() {
+    this.tabMenu.forEach((item, index) => {
+      item.addEventListener('click', () => this.activeTab(index));
     });
   }
 }

99cdce5c78cd9a10ca65c366d28aa9702d08d6f6
Gregory N. Mayer <gregory280@hotmail.com>
2022-05-07 19:43:19 -0300

    scroll animation better

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -2,23 +2,42 @@ export default class ScrollAnimation {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-    this.animationScroll = this.animationScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animationScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const issectionVisible = (sectionTop - this.windowMetade) < 0;
-      if (issectionVisible) {
-        section.classList.add('active');
-      } else if (section.classList.contains('active')) {
-        section.classList.remove('active');
+  // Pega a distância de cada item em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // Verifica a distância em cada objeto em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('active');
+      } else if (item.element.classList.contains('active')) {
+        item.element.classList.remove('active');
       }
     });
   }
 
+  // Remove o event de scroll
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
+  }
+
   init() {
-    this.animationScroll();
-    window.addEventListener('scroll', this.animationScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
   }
 }
